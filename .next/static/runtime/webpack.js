2d2e5220ed2d6ba0cf6f5f6b974c2dbc76a74ec6
franklindesign <manghi@gmail.com>
2020-01-02 13:32:41 -0800

    updated ayce menu

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f91e2862f7ebb427d7ca";
+/******/ 	var hotCurrentHash = "fdf8f090d55100b05e80";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
