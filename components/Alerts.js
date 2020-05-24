2c9db9def882af099e7aa75b126ec4337e68adbf
franklindesign <manghi@gmail.com>
2020-05-23 22:10:59 -0700

    hero offers updated

--- components/Alerts.js
+++ components/Alerts.js
@@ -6,8 +6,8 @@ export default function Alerts() {
       <Alert severity="info">
         <AlertTitle>Valued Patrons,</AlertTitle>
         We will continue to open our doors for take-out orders and delivery
-        only. Get free delivery with Uber Eats. 30% OFF discount for all
-        take-out orders.
+        only. Get free delivery with Uber Eats. 30% OFF discount for sushi,
+        sashimi, rolls, and appetizers.
       </Alert>
     </div>
   );
