ea56ec2b633b56427ef094fa578e42b566a0bd7d
franklindesign <manghi@gmail.com>
2022-01-17 02:15:34 -0800

    removed webpack config for svg

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   distDir: "build",
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
 
-    return config;
-  },
+  //   return config;
+  // },
 };
